78d242b99a3ea33376146f9400e964da8be29291
Jett Thompson <jett.thompson.59@gmail.com>
2022-10-30 21:02:54 +1100

    improve the finding of the assets directory

--- water-platformer-engine/src/main/java/app/cleancode/scaga/resources/ResourceReader.java
+++ water-platformer-engine/src/main/java/app/cleancode/scaga/resources/ResourceReader.java
@@ -10,13 +10,27 @@ import javax.imageio.ImageIO;
 
 public class ResourceReader {
   private static String assetsPrefix = "assets";
+
+  private static boolean isAssetsDirectory(String path) {
+    return Files.exists(Paths.get(path));
+  }
+
   static {
-    // For jpackaged apps, we assume the assets is under app/ because there is no way to configure
-    // it to use the root of the jpackage directory (our working directory).
-    boolean isJpackage = System.getProperty("jpackage.app-path") != null;
-    if (isJpackage) {
-      assetsPrefix = Path.of(System.getProperty("java.home")).resolveSibling("app")
-          .resolve("assets").toString();
+    if (isAssetsDirectory("assets")) {
+      assetsPrefix = "assets";
+    } else if (isAssetsDirectory("water-platformer-game/assets")) {
+      assetsPrefix = "water-platformer-game/assets";
+    } else {
+      Path jpackageDirectory = Path.of(System.getProperty("jpackage.app-path")).getParent();
+      Path siblingAppDirectory = jpackageDirectory.resolve("app");
+      Path parentSiblingAppDirectory = jpackageDirectory.resolveSibling("app");
+      if (isAssetsDirectory(siblingAppDirectory.resolve("assets").toString())) {
+        assetsPrefix = siblingAppDirectory.resolve("assets").toString();
+      } else if (isAssetsDirectory(parentSiblingAppDirectory.resolve("assets").toString())) {
+        assetsPrefix = parentSiblingAppDirectory.resolve("assets").toString();
+      } else {
+        throw new IllegalStateException("Can't find the assets root");
+      }
     }
   }
 
